dd9c07697d8b7acd0930745cca570021abc0b289
Laikh Tewari <laikhtewari1@gmail.com>
2015-07-15 11:11:41 -0700

    Added xcode proj

---
+++ Daily-Picture-Challenge/Daily-Picture-Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daily-Picture-Challenge
+//
+//  Created by Laikh Tewari on 7/15/15.
+//  Copyright (c) 2015 Laikh Tewari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
