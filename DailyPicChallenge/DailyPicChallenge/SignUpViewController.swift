8a1de7366ce52e21ffcfbcab7d2338cae61e4a3b
Laikh Tewari <laikhtewari1@gmail.com>
2015-08-06 15:53:28 -0700

    Fixed signing up

--- DailyPicChallenge/DailyPicChallenge/SignUpViewController.swift
+++ DailyPicChallenge/DailyPicChallenge/SignUpViewController.swift
@@ -16,6 +16,22 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPasswordField: UITextField!
     
+    @IBAction func hideUsernameTextWhenTapped(sender: UITextField) {
+        sender.text = ""
+    }
+    
+    @IBAction func hideEmailTextWhenTapped(sender: UITextField) {
+        sender.text = ""
+    }
+    
+    @IBAction func hidePasswordTextWhenTapped(sender: UITextField) {
+        sender.text = ""
+    }
+    
+    @IBAction func hideConfirmPasswordTextWhenTapped(sender: UITextField) {
+        sender.text = ""
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,46 +45,55 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func getStartedTapped(sender: AnyObject) {
-//        let userQuery = PFQuery(className: "User")
-//        var usernameArray: [String]!
-//        
-//        userQuery.findObjectsInBackgroundWithBlock {
-//            (results: [AnyObject]?, error: NSError?) -> Void in
-//            
-//            let userArray = results as? [PFUser] ?? []
-//            
-//            for user in userArray
-//            {
-//                usernameArray.append(user.username!)
-//            }
-//        }
-//        
-//        if let username = self.usernameField.text
-//        {
-//            var contained = false
-//            for obj in usernameArray
-//            {
-//                if obj == username
-//                {
-//                    contained = true
-//                }
-//            }
-//            if contained == false
-//            {
-//                self.T
-//            }
-//        }
-
+        let userQuery = PFUser.query()
+        userQuery?.whereKey("username", equalTo: usernameField.text)
+        userQuery?.findObjectsInBackgroundWithBlock {
+            (results:[AnyObject]?, error: NSError?) -> Void in
+            if results?.count == 0
+            {
+                if self.passwordField.text == self.confirmPasswordField.text
+                {
+                    let user = PFUser()
+                    user.username = self.usernameField.text
+                    user.password = self.passwordField.text
+                    user.email = self.emailField.text
+                    
+                    user.signUpInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                        
+                        if let error = error
+                        {
+                            let alertView = UIAlertView(title: "Error", message: "Unable to sign up", delegate: nil, cancelButtonTitle: "OK")
+                            alertView.show()
+                        }
+                        else
+                        {
+                            println("done saving")
+                            PFUser.logInWithUsernameInBackground(self.usernameField.text, password: self.passwordField.text, block: { (user: PFUser?, error: NSError?) -> Void in
+                                if let error = error
+                                {
+                                    let alertView = UIAlertView(title: "Error", message: "Unsuccessful login", delegate: nil, cancelButtonTitle: "OK")
+                                    alertView.show()
+                                }
+                                else
+                                {
+                                    println("logged in successfully")
+                                    self.performSegueWithIdentifier("signedUp", sender: self)
+                                }
+                            })
+                        }
+                    })
+                }
+                else
+                {
+                    let alertView = UIAlertView(title: "Error", message: "Passwords do not match", delegate: nil, cancelButtonTitle: "OK")
+                    alertView.show()
+                }
+            }
+            else
+            {
+                let alertView = UIAlertView(title: "Error", message: "Username already taken", delegate: nil, cancelButtonTitle: "OK")
+                alertView.show()
+            }
+        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
